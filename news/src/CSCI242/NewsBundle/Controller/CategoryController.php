3e3002319d897c037304e213dbbb91e07ebadecd
Brett Pittman <pittmana5@winthrop.edu>
2017-04-25 20:57:31 -0400

    Removed @Method("GET") from default controller above categoryIndexAction

--- news/src/CSCI242/NewsBundle/Controller/CategoryController.php
+++ news/src/CSCI242/NewsBundle/Controller/CategoryController.php
@@ -5,7 +5,8 @@ namespace CSCI242\NewsBundle\Controller;
 use CSCI242\NewsBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Category controller.
